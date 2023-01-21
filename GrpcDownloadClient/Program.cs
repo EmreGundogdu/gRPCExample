0c6b5fb4b40488c1250e1fd6990c6ba1c1a6e9c1
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-21 22:46:40 +0300

    Upload and download file operations added

---
+++ GrpcDownloadClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Net.Client;
+using grpcFileTransportClient;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7100");
+var client = new FileService.FileServiceClient(channel);
+
+string downloadPath = @"C:\Users\Emre\Desktop\FinalDev\gRPCExample\gRPCServer\GrpcDownloadClient\DownloadFiles";
+var fileInfo = new grpcFileTransportClient.FileInfo
+{
+    FileExtension = ".rar",
+    FileName = "Adobe Acrobat Pro DC 2020.009.20074 FUL SÜRUM"
+};
+FileStream fileStream = null;
+var request = client.FileDownload(fileInfo);
+CancellationTokenSource cancellationToken = new CancellationTokenSource();
+int count = 0;
+decimal chunkSize = 0;
+while (await request.ResponseStream.MoveNext(cancellationToken.Token))
+{
+    if (count++ == 0)
+    {
+        fileStream = new FileStream(@$"{downloadPath}\{request.ResponseStream.Current.Info.FileName}{request.ResponseStream.Current.Info.FileExtension}", FileMode.CreateNew);
+        fileStream.SetLength(request.ResponseStream.Current.FileSize);
+    }
+    var buffer = request.ResponseStream.Current.Buffer.ToByteArray();
+    await fileStream.WriteAsync(buffer, 0, request.ResponseStream.Current.ReadedByte);
+    Console.WriteLine($"{Math.Round(((chunkSize += request.ResponseStream.Current.ReadedByte) * 100) / request.ResponseStream.Current.FileSize)}%");
+    Console.WriteLine("Yüklendi");
+    await fileStream.DisposeAsync();
+    fileStream.Close();
+}
---
+++ GrpcUploadClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using Google.Protobuf;
+using Grpc.Net.Client;
+using grpcFileTransportClient;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7100");
+var client = new FileService.FileServiceClient(channel);
+
+string file = @"C:\Users\Emre\Downloads\Adobe Acrobat Pro DC 2020.009.20074 FUL SÜRUM.rar";
+FileStream fileStream = new(file,FileMode.Open);
+var content = new BytesContent
+{
+    FileSize = fileStream.Length,
+    ReadedByte = 0,
+    Info = new grpcFileTransportClient.FileInfo() { FileName = Path.GetFileNameWithoutExtension(fileStream.Name), FileExtension = Path.GetExtension(fileStream.Name) }
+};
+
+var uploadResponse = client.FileUpload();
+byte[] buffer = new byte[2048];
+while ((content.ReadedByte =await fileStream.ReadAsync(buffer,0,buffer.Length))>0)
+{
+    content.Buffer = ByteString.CopyFrom(buffer);
+    await uploadResponse.RequestStream.WriteAsync(content);
+}
+await uploadResponse.RequestStream.CompleteAsync();
+fileStream.Close();
